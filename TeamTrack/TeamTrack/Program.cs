ccc9ede9e385b86363dea5969aac84f1114e53a6
akrahman76 <42632725+akrahman76@users.noreply.github.com>
2026-01-12 21:10:15 +0600

    Register identity

--- TeamTrack/TeamTrack/Program.cs
+++ TeamTrack/TeamTrack/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using TeamTrack.Infrastructure.Persistence;
+using TeamTrack.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +15,16 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 6;
+    });
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
